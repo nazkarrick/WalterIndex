8961a62dc7af74dce9d2281e509851a8d9bbee72
RJ G <nazkarrikk@RJs-MacBook-Air.local>
2021-04-04 17:17:33 -0400

    Navbar link: ISS connected

--- api/application.py
+++ api/application.py
@@ -6,15 +6,21 @@ DEBUG = True
 PORT = 5000
 app = Flask(__name__)
 
+
+
+#########=== Navigation Bar ===#########
+
 @app.route('/', methods=['GET'])
 def index():
     api_nasa = requests.get('https://api.nasa.gov/planetary/apod?api_key=WWWXENdMExrIHV2WTMh3baouTEuBpkcmrQqRZtb8')
     res = json.loads(api_nasa.content)
-    return render_template('index.html', res=res)
+    return render_template('apod.html', res=res)
 
-@app.route('/apod')
-def get_apod():
-    return {"apod": "apod data"}
+@app.route('/iss', methods=['GET'])
+def get_iss():
+    issLoc = requests.get('http://api.open-notify.org/iss-now.json')
+    issRes = json.loads(issLoc.content)
+    return render_template('iss.html', issRes=issRes )
 
 if __name__ == '__main__':
     app.run(debug=DEBUG, port=PORT)
